d45444e29f784ba19902dec416c329ad717d1cc2
Strahil731 <hristovstrahil5@gmail.com>
2025-11-30 17:26:25 +0200

    Create Трекинг мания.js

---
+++ Трекинг мания.js
@@ -0,0 +1,26 @@
+function trekkingMania(input) {
+    let numberGroup = Number(input.shift());
+    let allPeople = 0;
+    let musalaPeople = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        let currentGroup = Number(input[i]);
+
+        
+}
+
+trekkingMania
+    (
+        ["10",
+            "10",
+            "5",
+            "1",
+            "100",
+            "12",
+            "26",
+            "17",
+            "37",
+            "40",
+            "78"
+        ]
+    )
